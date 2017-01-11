65997894183decc32c1677303c7068421f30b988
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-11 12:35:19 +0100

    Inicjalizacja obiektów za pomocą przypisywania

--- CBB.HelpDesk.ConsoleClient/Program.cs
+++ CBB.HelpDesk.ConsoleClient/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using CBB.HelpDesk.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,37 @@ namespace CBB.HelpDesk.ConsoleClient
     {
         static void Main(string[] args)
         {
+            AddTicketTest();
+
+            Console.WriteLine("Press any key to exit.");
+
+            Console.ReadKey();
+
+        }
+
+        private static void AddTicketTest()
+        {
+            User user = new User();
+            user.UserId = 1;
+            user.FirstName = "Marcin";
+            user.LastName = "Sulecki";
+            user.IsActive = true;
+
+            Category category = new Category();
+            category.CategoryId = 1;
+            category.Name = "IT";
+            category.IsActive = true;
+
+            Ticket issue = new Ticket();
+            issue.TicketId = 1;
+            issue.CreateDate = DateTime.Now;
+            issue.CreateUser = user;
+            issue.Priority = Priority.Normal;
+            issue.Category = category;
+
+
+            Console.WriteLine(user.FirstName);
+            Console.WriteLine(user.LastName);
         }
     }
 }
