9e4aab91904553e2509003ee3672723110e8ded0
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-12 13:12:01 +0100

    Przykładowa implementacja interfejsu do obsługi Ticketów - wyświetlenie na konsoli

---
+++ CBB.HelpDesk.AbcBankServices/AbcBankTicketsService.cs
@@ -0,0 +1,58 @@
+﻿using CBB.HelpDesk.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CBB.HelpDesk.Models;
+
+namespace CBB.HelpDesk.AbcBankServices
+{
+    public class AbcBankTicketsService : ITicketsService
+    {
+        public void Add(Ticket ticket)
+        {
+            Console.WriteLine(ticket);
+        }
+
+        public Ticket Get(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Ticket ticket)
+        {   
+            throw new NotImplementedException();
+        }
+
+        public void Send(Ticket ticket)
+        {
+            if (ticket==null)
+            {
+                throw new ArgumentNullException("ticket");
+            }
+
+            if (string.IsNullOrEmpty(ticket.Title))
+            {
+                throw new ArgumentNullException("Title");
+            }
+
+            if (ticket.CreateDate < DateTime.Today)
+            {
+                throw new ArgumentOutOfRangeException("CreateDate");
+            }
+
+            Console.WriteLine("Przygotowanie bramki");
+
+            Console.WriteLine("Obliczeie kosztu wysłania");
+
+
+            Console.WriteLine($"Sending sms... {ticket.Title}");
+        }
+    }
+}
