53a419a9caec4d3e30cad64737625ea4ebb47e21
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-12 13:11:17 +0100

    Implementacja interfejsu do obsługi Ticketów - zapis do pliku

---
+++ CBB.HelpDesk.PekaoServices/PekaoTicketsService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using CBB.HelpDesk.Interfaces;
+using CBB.HelpDesk.Models;
+using System.IO;
+
+namespace CBB.HelpDesk.PekaoServices
+{
+    public class PekaoTicketsService : ITicketsService
+    {
+        public void Add(Ticket ticket)
+        {
+            var stream = new StreamWriter("tickets.txt", true);
+
+            stream.WriteLine($"{ticket.TicketId} | {ticket.Title} | {ticket.Description}");
+
+            stream.Close();
+        }
+
+        public Ticket Get(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Remove(int ticketId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Send(Ticket ticket)
+        {
+            throw new NotSupportedException();
+        }
+
+        public void Update(Ticket ticket)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
