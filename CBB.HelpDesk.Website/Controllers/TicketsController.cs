94df4bebf8c8627b52d3463a137ca70dfc9f5500
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-13 16:01:08 +0100

    Utworzenie strony webowej

---
+++ CBB.HelpDesk.Website/Controllers/TicketsController.cs
@@ -0,0 +1,23 @@
+﻿using CBB.HelpDesk.Interfaces;
+using CBB.HelpDesk.PekaoServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace CBB.HelpDesk.Website.Controllers
+{
+    public class TicketsController : Controller
+    {
+        ITicketsService TicketsService = new PekaoTicketsService();
+
+        // GET: Tickets
+        public ActionResult Index()
+        {
+            var tickets = TicketsService.Get();
+
+            return View(tickets);
+        }
+    }
+}
---
+++ CBB.HelpDesk.Website/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="CBB.HelpDesk.Website.MvcApplication" Language="C#" %>
