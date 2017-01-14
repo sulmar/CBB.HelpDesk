0ee0854d69d4e773664c3c1c353bd38b422384aa
Marcin Sulecki <marcin.sulecki@gmail.com>
2017-01-14 10:37:36 +0100

    Dodanie walidacji za pomocą FluentValidation

---
+++ CBB.HelpDesk.Models/Validators/TicketValidator.cs
@@ -0,0 +1,27 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CBB.HelpDesk.Models.Validators
+{
+
+
+    // FluentValidation
+    public class TicketValidator : AbstractValidator<Ticket>
+    {
+        public TicketValidator()
+        {
+            RuleFor(ticket => ticket.Title)
+                .NotEmpty()
+                .Length(1, 6);
+
+            RuleFor(ticket => ticket.Description)
+                .NotEmpty();
+
+
+        }
+    }
+}
